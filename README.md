fedd5b8081fb1e98a86dbc941e31dc7a7797c04e
upsatwal <ups@atwal.info>
2023-07-08 02:21:29 +0530

    all is working

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # gallerypage-clean
+
 rewrite gallery page from the ground up for MLSC TIET
---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import Gallery from "./pages/Gallery/gallery.js";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+  <>
+  <Gallery/>
+  </>
+  );
+}
+
+export default App;
